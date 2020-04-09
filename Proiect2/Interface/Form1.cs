df8e04dfdc83680824835847543292960e6476bd
MayaMaria <mariabulboaca@yahoo.com>
2020-04-09 19:18:31 +0300

    Adaugare documentatie

--- Proiect2/Interface/Form1.cs
+++ Proiect2/Interface/Form1.cs
@@ -30,7 +30,10 @@ namespace Interface
         private void uploadButton_Click(object sender, EventArgs e)
         {
             OpenFileDialog result = new OpenFileDialog();
-            result.Filter = "Image Files | *.jpg; *.jpeg; *.jpe; *.jfif; *.png ;*.PNG;| Video files |*.wav;*.aac;*.wma;*.wmv;*.avi;*.mpg;*.mpeg;*.m1v;*.mp2;*.mp3;*.mpa;*.mpe;*.m3u;*.mp4;*.mov;*.3g2;*.3gp2;*.3gp;*.3gpp;*.m4a;*.cda;*.aif;*.aifc;*.aiff;*.mid;*.midi;*.rmi;*.mkv;*.WAV;*.AAC;*.WMA;*.WMV;*.AVI;*.MPG;*.MPEG;*.M1V;*.MP2;*.MP3;*.MPA;*.MPE;*.M3U;*.MP4;*.MOV;*.3G2;*.3GP2;*.3GP;*.3GPP;*.M4A;*.CDA;*.AIF;*.AIFC;*.AIFF;*.MID;*.MIDI;*.RMI;*.MKV";
+            result.Filter = "Image Files | *.jpg; *.jpeg; *.jpe; *.jfif; *.png ;*.PNG;| Video files |*.wav;*.aac;*.wma;*.wmv;" +
+                "*.avi;*.mpg;*.mpeg;*.m1v;*.mp2;*.mp3;*.mpa;*.mpe;*.m3u;*.mp4;*.mov;*.3g2;*.3gp2;*.3gp;*.3gpp;*.m4a;*.cda;" +
+                "*.aif;*.aifc;*.aiff;*.mid;*.midi;*.rmi;*.mkv;*.WAV;*.AAC;*.WMA;*.WMV;*.AVI;*.MPG;*.MPEG;*.M1V;*.MP2;*.MP3;" +
+                "*.MPA;*.MPE;*.M3U;*.MP4;*.MOV;*.3G2;*.3GP2;*.3GP;*.3GPP;*.M4A;*.CDA;*.AIF;*.AIFC;*.AIFF;*.MID;*.MIDI;*.RMI;*.MKV";
             if (result.ShowDialog() == DialogResult.OK)
             {
                 string file = result.FileName;
@@ -92,8 +95,6 @@ namespace Interface
         private string getTypeForFile(string extension)
         {
             string[] photoExtensions = new string[] { "jpg", "jpeg", "jpe", "jfif", "png", "PNG" };
-            string[] videoExtensions = new string[] { "wav","aac","wma","wmv","avi", "mpg","mpeg", 
-            "mp4","mov","m4a", "WAV","AAC","WMA","WMV","AVI"};
 
             if (photoExtensions.Contains(extension))
                 return "Photo";
